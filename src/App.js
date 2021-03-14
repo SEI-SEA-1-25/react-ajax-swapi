710299085f336d27d6649c6c09ada77790ca905c
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-13 16:35:22 -0800

    init set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//SET-UP//
+import "./App.css";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+//Route-Imports//
+
+function App() {
+  return (
+    <div>
+      <h1>🪐 Star Wars React Deliverable 🪐</h1>
+    </div>
+  );
+}
+export default App;

f4d99d7b551760d97e44e5cdc29f2ddf106df258
Jcom619 <joshuacompton@my.smccd.edu>
2021-03-15 10:13:31 -0700

    j

---
+++ react-jajx-swapi/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="clouds">
+        <h1>Starfighter oi</h1>
+        <img src={logo} className="neonShadow App-logo" alt="logo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-jajx-swapi/src/partials/gallery.js
@@ -0,0 +1,86 @@
+import { Components } from "react";
+
+starshipArray = [
+  "Executor",
+  "X-Wing",
+  " Y-Wing,Imperial Shuddle",
+  " Millinium Falcon",
+  "Death Star",
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Starship </h1>
+        <main>
+<div class="content">
+  <h1>Responsive image gallery</h1>
+  <div class="gallery">
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Plant" />
+      </a>
+      <figcaption>Plant</figcaption>
+    </figure>
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Man with facial tattoo" />
+      </a>
+      <figcaption>Man with facial tattoo</figcaption>
+    </figure>
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Leasure" />
+      </a>
+      <figcaption>Leasure</figcaption>
+    </figure>
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Pretty colors" />
+      </a>
+      <figcaption>Pretty colors</figcaption>
+    </figure>
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Architecture" />
+      </a>
+      <figcaption>Architecture</figcaption>
+    </figure>
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Beach" />
+      </a>
+      <figcaption>Beach</figcaption>
+    </figure>
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Mountains" />
+      </a>
+      <figcaption>Mountains</figcaption>
+    </figure>
+   a href="#">
+        <img src="#" alt="Starry sky" />
+      </a>
+      <figcaption>Starry sky</figcaption>
+    </figure> <figure class="galleryItem">
+      <
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="Forest" />
+      </a>
+      <figcaption>Forest</figcaption>
+    </figure>
+    <figure class="galleryItem">
+      <a href="#">
+        <img src="#" alt="City" />
+      </a>
+      <figcaption>City</figcaption>
+    </figure>
+  </div>
+</div>
+</main>
+      </div>
+    );
+  }
+}
